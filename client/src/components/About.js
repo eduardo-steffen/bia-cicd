08b4e9eb8b915c347d328d201e9904a0f083d1f5
eduardo-steffen <eduardosteffen@gmail.com>
2023-04-17 01:53:43 +0000

    ajustando msg para teste de pipeline

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,7 @@ const About = () => {
   return (
     <div>
       <h4>Versão 1.0.3</h4>
-      <h5>BIA 02 a 05 de Maio/2022</h5>
+      <h5>BIA 02 a 05 de Maio/2023</h5>
       <Link to="/">Voltar</Link>
     </div>
   );
